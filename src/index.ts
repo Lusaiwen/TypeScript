1779633f754294cf6ce5cca5b0ff354ade5d03b1
Lusaiwen <1811977648@qq.com>
2021-12-26 15:41:18 +0800

    2.基本类型检查

--- src/index.ts
+++ src/index.ts
@@ -1,97 +1,58 @@
+//poker
 
-
-function add(num1: number, num2: number) {
-    return num1 + num2;
-}
-
-let name = add(1, 3);
-console.log(name);
-
-let str: undefined | string = 'fsdfsdfdsg';
-
-function throwError(msg: string): never {
-    throw new Error(msg);
-}
-
-function continueFunc(): never {
-    while (true) {}
-}
-
-//字面量类型
-let gender: '男' | '女';
-gender = '男';
-
-let arr: []; //表示为该数组只能为空数组
-
-let stu: {
-    name: string;
-    age: number;
+type Deck = NormalCard[];
+type Color = '♥' | '♠' | '♣' | '♦';
+type NormalCard = {
+    mark: number;
+    color: Color;
 };
 
-//元组类型
-let arr2: [string, number];
-arr2 = ['fds', 234];
-
-//类型别名
-type Gender = '男' | '女';
-type User = {
-    name: string;
-    age: number;
-};
-
-let user: User;
-function getUsers(g: Gender): User[] {
-    return [];
-}
-
-//函数的相关约束
-/**
- * return a * b
- * @param a 
- * @param b 
- */
-function combine(a: number, b: number): number;
-/**
- * return a + b
- * @param a 
- * @param b 
- */
-function combine(a: string, b: string): string;
-
-function combine(a: number | string, b: number | string): number | string {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return a * b;
-    } else if (typeof a === 'string' && typeof b === 'string') {
-        return a + b;
+function createDeck(): Deck {
+    let deck: Deck = [];
+    for (let i = 1; i <= 13; i++) {
+        deck.push({
+            color: '♠',
+            mark: i
+        });
+        deck.push({
+            color: '♣',
+            mark: i
+        });
+        deck.push({
+            color: '♥',
+            mark: i
+        });
+        deck.push({
+            color: '♦',
+            mark: i
+        });
     }
-    throw new Error('a and b must be same');
+    return deck;
 }
 
-combine(3, 3);
-
-//函数的可选参数
-/**
- * 
- * @param a 
- * @param b 
- * @param c ?可以不选   c:number : undefined
- * @returns 
- */
-function sum(a:number , b:number , c?:number){
-    if(c){
-        return a + b +c ;
-    }else {
-        return a + b;
-    }
+function printDeck(deck: Deck) {
+    let result = '\n';
+    deck.forEach((card, i) => {
+        let str = card.color;
+        if (card.mark <= 10) {
+            str += card.mark;
+        }
+        if (card.mark === 11) {
+            str += 'J';
+        }
+        if (card.mark === 12) {
+            str += 'Q';
+        }
+        if (card.mark === 13) {
+            str += 'K';
+        }
+        result += str + '\t';
+        if ((i + 1) % 4 === 0) {
+            result += '\n';
+        }
+    });
+    console.log(result);
 }
-//默认值   
-function sum1(a:number , b:number , c:number = 0){
-    if(c){
-        return a + b +c ;
-    }else {
-        return a + b;
-    }
-}
-sum1(1,2);//当不传第三个值时，c表示可选参数
 
-
+const deck = createDeck();
+printDeck(deck);
---
+++ src/index2.ts
@@ -0,0 +1,97 @@
+
+
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+let name = add(1, 3);
+console.log(name);
+
+let str: undefined | string = 'fsdfsdfdsg';
+
+function throwError(msg: string): never {
+    throw new Error(msg);
+}
+
+function continueFunc(): never {
+    while (true) {}
+}
+
+//字面量类型
+let gender: '男' | '女';
+gender = '男';
+
+let arr: []; //表示为该数组只能为空数组
+
+let stu: {
+    name: string;
+    age: number;
+};
+
+//元组类型
+let arr2: [string, number];
+arr2 = ['fds', 234];
+
+//类型别名
+type Gender = '男' | '女';
+type User = {
+    name: string;
+    age: number;
+};
+
+let user: User;
+function getUsers(g: Gender): User[] {
+    return [];
+}
+
+//函数的相关约束
+/**
+ * return a * b
+ * @param a 
+ * @param b 
+ */
+function combine(a: number, b: number): number;
+/**
+ * return a + b
+ * @param a 
+ * @param b 
+ */
+function combine(a: string, b: string): string;
+
+function combine(a: number | string, b: number | string): number | string {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a * b;
+    } else if (typeof a === 'string' && typeof b === 'string') {
+        return a + b;
+    }
+    throw new Error('a and b must be same');
+}
+
+combine(3, 3);
+
+//函数的可选参数
+/**
+ * 
+ * @param a 
+ * @param b 
+ * @param c ?可以不选   c:number : undefined
+ * @returns 
+ */
+function sum(a:number , b:number , c?:number){
+    if(c){
+        return a + b +c ;
+    }else {
+        return a + b;
+    }
+}
+//默认值   
+function sum1(a:number , b:number , c:number = 0){
+    if(c){
+        return a + b +c ;
+    }else {
+        return a + b;
+    }
+}
+sum1(1,2);//当不传第三个值时，c表示可选参数
+
+
