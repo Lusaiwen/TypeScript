9315b7abf9764923245f45024ee041553a998c64
Lusaiwen <1811977648@qq.com>
2022-01-24 13:39:54 +0800

    3.扩展类型 - 枚举

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,45 @@
 //poker
 
 type Deck = NormalCard[];
-type Color = '♥' | '♠' | '♣' | '♦';
+enum Color {
+    heart = '♥',
+    spade = '♠',
+    club = '♣',
+    diamond = '♦'
+}
+
+enum Mark {
+    A = 'A',
+    Two = '2',
+    Three = '3',
+    Four = '4',
+    Five = '5',
+    Six = '6',
+    Seven = '7',
+    Eight = '8',
+    Nine = '9',
+    Ten = '10',
+    Eleven = 'J',
+    Twelve = 'Q',
+    Thirteen = 'K'
+}
+
 type NormalCard = {
-    mark: number;
+    mark: Mark;
     color: Color;
 };
 
 function createDeck(): Deck {
     let deck: Deck = [];
-    for (let i = 1; i <= 13; i++) {
-        deck.push({
-            color: '♠',
-            mark: i
-        });
-        deck.push({
-            color: '♣',
-            mark: i
-        });
-        deck.push({
-            color: '♥',
-            mark: i
-        });
-        deck.push({
-            color: '♦',
-            mark: i
-        });
+    const marks = Object.values(Mark);
+    const colors = Object.values(Color);
+    for (const m of marks) {
+        for (const c of colors) {
+            deck.push({
+                mark: m,
+                color: c
+            });
+        }
     }
     return deck;
 }
@@ -33,19 +47,7 @@ function createDeck(): Deck {
 function printDeck(deck: Deck) {
     let result = '\n';
     deck.forEach((card, i) => {
-        let str = card.color;
-        if (card.mark <= 10) {
-            str += card.mark;
-        }
-        if (card.mark === 11) {
-            str += 'J';
-        }
-        if (card.mark === 12) {
-            str += 'Q';
-        }
-        if (card.mark === 13) {
-            str += 'K';
-        }
+        let str = card.color + card.mark;
         result += str + '\t';
         if ((i + 1) % 4 === 0) {
             result += '\n';
