2bef9df9dd5c6cc08b84d5ec9b9e40c4186a0efa
Lusaiwen <1811977648@qq.com>
2022-02-03 15:41:14 +0800

    4.模块化

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,6 @@
-//poker
+import getTime, { sum, a } from './myModule';
 
-type Deck = NormalCard[];
-enum Color {
-    heart = '♥',
-    spade = '♠',
-    club = '♣',
-    diamond = '♦'
-}
+console.log(sum(1, 2));
+console.log(a);
 
-enum Mark {
-    A = 'A',
-    Two = '2',
-    Three = '3',
-    Four = '4',
-    Five = '5',
-    Six = '6',
-    Seven = '7',
-    Eight = '8',
-    Nine = '9',
-    Ten = '10',
-    Eleven = 'J',
-    Twelve = 'Q',
-    Thirteen = 'K'
-}
-
-type NormalCard = {
-    mark: Mark;
-    color: Color;
-};
-
-function createDeck(): Deck {
-    let deck: Deck = [];
-    const marks = Object.values(Mark);
-    const colors = Object.values(Color);
-    for (const m of marks) {
-        for (const c of colors) {
-            deck.push({
-                mark: m,
-                color: c
-            });
-        }
-    }
-    return deck;
-}
-
-function printDeck(deck: Deck) {
-    let result = '\n';
-    deck.forEach((card, i) => {
-        let str = card.color + card.mark;
-        result += str + '\t';
-        if ((i + 1) % 4 === 0) {
-            result += '\n';
-        }
-    });
-    console.log(result);
-}
-
-const deck = createDeck();
-printDeck(deck);
+console.log(getTime());
---
+++ src/myModule.ts
@@ -0,0 +1,9 @@
+export function sum(a: number, b: number) {
+    return a + b;
+}
+
+export const a: number = 1;
+
+export default function getTime() {
+    console.log(Date.now());
+}
