beca8605261f4930d3db8d6fa62e8aa7fd9b1354
Lusaiwen <1811977648@qq.com>
2022-10-04 18:23:16 +0800

    2.基本类型检查

---
+++ src/index3.js
@@ -0,0 +1,4 @@
+function sum1(a, b) {
+    return a + b;
+}
+var total = sum1(1, 2);
---
+++ src/index3.ts
@@ -0,0 +1,4 @@
+function sum1(a: number, b: number): number {
+  return a + b;
+}
+let total: number = sum1(1, 2);
